a2216012a14ded02687b8bf3445005721c270d74
group36 <korlan01@saintmarys.edu>
2021-12-07 18:24:09 -0600

    Issue not present in this configuration, need to add SpringConnectionProvider and retest

--- querydsl-examples/querydsl-example-sql-spring/src/test/java/com/querydsl/example/dao/CustomerDaoTest.java
+++ querydsl-examples/querydsl-example-sql-spring/src/test/java/com/querydsl/example/dao/CustomerDaoTest.java
@@ -3,6 +3,7 @@ package com.querydsl.example.dao;
 import com.querydsl.example.dto.Customer;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.annotations.PropertyType;
+import com.querydsl.sql.SQLQueryFactory;
 //import com.querydsl.core.types.dsl.numberTemplate;
 //import org.junit.core.type.Expressions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,59 +42,26 @@ public class CustomerDaoTest extends AbstractDaoTest {
     }
 
     /*
-    *Test submitted in issue is commented out below
+    * Begin added tests:
     * -problem was "1L" an input was returning null pointer exception instead of incompatible type
     * -"1L" is a quoted string
     */
 
-    /*
+    
     @Test
     //received correct "incompatible type" error when using findbyId of quoted string
+    //testing inside of a prewritten function
     public void findById_string() {
 
-        assertNotNull(customerDao.findById("'1L'"));
-    }*/
-
-    //throw in a test for a field/select that wouldn't exist
-
-    @Test
-    public void mod_findAll() {
-        //Customer customer = new Customer();
-        //val num = Expressions.numberTemplate(Long.class, "'1L'");
-        //val num = Expressions.numberTemplate(Long,"'1L'");
-        List<Customer> customers = queryFactory.select(Expressions.numberTemplate(Long.class,"'1L'")).from(customerDao).fetchOne();
-        //List<Customer> customers = customerDao.findAll(Expressions.numberTemplate(Long.class,"'1L'"));
-        assertFalse(customers.isEmpty());
+        assertNotNull(customerDao.findById(Expressions.numberTemplate(Long.class,"'1L'")));
     }
 
-    /*
+    //received correct "incompatible type" error when writing a query function from scratch
     @Test
-    public void find_list_by_predicate() {
-        Tweet tw1 = new Tweet();
-        tw1.setPosterId(posterId);
-        tw1.setContent("It is a alive! #YOLO");
-        repository.save(tw1);
-
-        Tweet tw2 = new Tweet();
-        tw2.setPosterId(posterId);
-        tw2.setContent("Oh the humanity!");
-        repository.save(tw2);
-
-        Tweet tw3 = new Tweet();
-        tw3.setPosterId(posterId);
-        tw3.setContent("#EpicFail");
-        repository.save(tw3);
-
-        assertEquals(1, repository.findAll(tweet.content.contains("#YOLO")).size());
-    }*/
-
-    /*
-    @Test
-    fun `unexpectedly throws NPE on incompatible type`() {
-    val num = Expressions.numberTemplate(Long::class.java, "'1L'")
-    sql.select(num)
-        .from(Expressions.stringTemplate("dual"))
-        .fetch()
-}*/
+    public void mod_findAll() {
+        SQLQueryFactory queryFactory;
+        List<Customer> customers = queryFactory.from(customerDao).select(Expressions.numberTemplate(Long.class,"'1L'")).fetch();
+        assertFalse(customers.isEmpty());
+    }
 
 }
